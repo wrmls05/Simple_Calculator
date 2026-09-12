b311781485e26539c482728130191186fd87d1e5
Owen <woramet.le@ku.th>
2026-09-13 00:51:46 +0700

    Add delete button functionality and update calculator layout for improved usability

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -3,6 +3,7 @@
     let buttons = document.querySelectorAll('[data-action="input"]');
     let clear = document.querySelector('[data-action="clear"]');
     let equal = document.querySelector('[data-action="equal"]');
+    let deleteBtn = document.querySelector('[data-action="delete"]');
 
     buttons.forEach(function (button) {
         button.addEventListener('click', function () {
@@ -23,5 +24,9 @@
 
     clear.addEventListener('click', function () {
         screen.value = '';
+    })
+
+    deleteBtn.addEventListener('click', function () {
+        screen.value = screen.value.slice(0, -1);
     })
 })();
